006169c361bb8250a25a11adbbdfaa3c88d7c998
Jaroslaw Sierant <jaroslaw.sierant@gmail.com>
2016-11-12 13:18:16 +0100

    trailing spaces removed

--- init.el
+++ init.el
@@ -130,32 +130,32 @@ Return a list of installed packages or nil for every skipped package."
 (global-evil-matchit-mode 1)
 
 
-(require 'helm)                                                                                                        
-(global-set-key (kbd "C-c h") 'helm-command-prefix)                                                                    
-(global-unset-key (kbd "C-x c"))                                                                                       
-(global-set-key (kbd "M-x") 'helm-M-x)                                                                                 
-(global-set-key (kbd "C-x b") 'helm-mini)                                                                              
-(global-set-key (kbd "C-x C-b") 'helm-buffers-list)                                                                    
-(global-set-key (kbd "C-x C-f") 'helm-find-files)                                                                      
+(require 'helm)
+(global-set-key (kbd "C-c h") 'helm-command-prefix)
+(global-unset-key (kbd "C-x c"))
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-x b") 'helm-mini)
+(global-set-key (kbd "C-x C-b") 'helm-buffers-list)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
 (evil-leader/set-key
   "l b" 'helm-buffers-list)
 
-(setq                                                                                                                  
- helm-split-window-in-side-p           t                                                                               
-   ; open helm buffer inside current window,                                                                           
-   ; not occupy whole other window                                                                                     
- helm-move-to-line-cycle-in-source     t                                                                               
-   ; move to end or beginning of source when                                                                           
-   ; reaching top or bottom of source.                                                                                 
- helm-ff-search-library-in-sexp        t                                                                               
-   ; search for library in `require' and `declare-function' sexp.                                                      
- helm-scroll-amount                    8                                                                               
-   ; scroll 8 lines other window using M-<next>/M-<prior>                                                              
- helm-ff-file-name-history-use-recentf t                                                                               
- ;; Allow fuzzy matches in helm semantic                                                                               
- helm-semantic-fuzzy-match t                                                                                           
- helm-imenu-fuzzy-match    t)                                                                                          
-;; Have helm automaticaly resize the window                                                                            
+(setq
+ helm-split-window-in-side-p           t
+   ; open helm buffer inside current window,
+   ; not occupy whole other window
+ helm-move-to-line-cycle-in-source     t
+   ; move to end or beginning of source when
+   ; reaching top or bottom of source.
+ helm-ff-search-library-in-sexp        t
+   ; search for library in `require' and `declare-function' sexp.
+ helm-scroll-amount                    8
+   ; scroll 8 lines other window using M-<next>/M-<prior>
+ helm-ff-file-name-history-use-recentf t
+ ;; Allow fuzzy matches in helm semantic
+ helm-semantic-fuzzy-match t
+ helm-imenu-fuzzy-match    t)
+;; Have helm automaticaly resize the window
 (helm-autoresize-mode 1)
 
 
