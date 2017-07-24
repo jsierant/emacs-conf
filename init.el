2d89d1789903299800a574c659b01c88c4f9b5c5
Jaroslaw Sierant <jaroslaw.sierant@gmail.com>
2017-07-24 21:36:22 +0200

    powerline added

--- init.el
+++ init.el
@@ -72,6 +72,22 @@
 (use-package darktooth-theme
   :config (init-theme))
 
+(use-package powerline
+  :config
+ (setq powerline-utf-8-separator-left        #xe0b0
+       powerline-utf-8-separator-right       #xe0b2
+       airline-utf-glyph-separator-left      #xe0b0
+       airline-utf-glyph-separator-right     #xe0b2
+       airline-utf-glyph-subseparator-left   #xe0b1
+       airline-utf-glyph-subseparator-right  #xe0b3
+       airline-utf-glyph-branch              #xe0a0
+       airline-utf-glyph-readonly            #xe0a2
+       airline-utf-glyph-linenumber          #xe0a1)
+)
+(use-package airline-themes
+  :config (load-theme 'airline-cool t)
+  )
+
 
 ;; ;; packages
 ;; (require 'package)
